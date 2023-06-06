392c44b95c53d3de0471b94e1c73ea9a63c0cdd4
hichame kabiri <38048537+thchi@users.noreply.github.com>
2023-06-06 13:41:53 +0200

    Update AMadam.py

--- cifar_imagenet/utils/AMadam.py
+++ cifar_imagenet/utils/AMadam.py
@@ -330,9 +330,9 @@ class AdamW(Optimizer):
 
                 state['step'] += 1
 
-                #exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
                 exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                exp_avg_sq=torch.abs(exp_avg)
+               
                 denom = exp_avg_sq.sqrt().add_(group['eps'])
                 bias_correction1 = 1 - beta1 ** state['step']
                 bias_correction2 = 1 - beta2 ** state['step']
